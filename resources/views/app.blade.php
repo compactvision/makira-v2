afb035818324a8105716712eb05018e7be53f1b4
compactvision <heymytechcorporation@gmail.com>
2025-12-18 12:29:35 +0100

    Build new version to deploy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
 
         <link rel="icon" href="/images/logo.png" sizes="any">
         <link rel="icon" href="/images/logo.png" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="/images/logo.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
